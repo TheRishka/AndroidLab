3617f1349ed8da0fc298bfb2292fd9a24d11c3d8
Rishad Mustafaev <rishad.mustafaev@dz.ru>
2016-09-15 13:26:41 +0300

    Sample with layouts

--- app/src/main/java/com/therishka/androidlab/MainActivity.java
+++ app/src/main/java/com/therishka/androidlab/MainActivity.java
@@ -1,7 +1,10 @@
 package com.therishka.androidlab;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button toRelative = (Button) findViewById(R.id.to_relative);
+        Button toLinear = (Button) findViewById(R.id.to_linear);
+
+        toRelative.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleClicks(view.getId());
+            }
+        });
+
+        toLinear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleClicks(view.getId());
+            }
+        });
+
+
+    }
+
+    private void handleClicks(@IdRes int buttonId) {
+        switch (buttonId) {
+            case R.id.to_linear:
+                ScreenDelegate.startScreen(ScreenDelegate.Screens.LINEAR_LAYOUT_SCREEN, this);
+                break;
+            case R.id.to_relative:
+                ScreenDelegate.startScreen(ScreenDelegate.Screens.RELATIVE_LAYOUT_SCREEN, this);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/therishka/androidlab/ScreenDelegate.java
@@ -0,0 +1,36 @@
+package com.therishka.androidlab;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+
+/**
+ * @author Rishad Mustafaev.
+ */
+public class ScreenDelegate {
+
+    public enum Screens {
+        LINEAR_LAYOUT_SCREEN,
+        RELATIVE_LAYOUT_SCREEN,
+    }
+
+
+    public static void startScreen(@NonNull Screens screen, @NonNull Activity activity) {
+        activity.startActivity(getRequiredIntent(screen, activity));
+    }
+
+    private static Intent getRequiredIntent(@NonNull Screens screen, @NonNull Activity activity) {
+        Intent screenIntent;
+        switch (screen) {
+            case LINEAR_LAYOUT_SCREEN:
+                screenIntent = new Intent(activity, LinearLayoutActivity.class);
+                break;
+            case RELATIVE_LAYOUT_SCREEN:
+                screenIntent = new Intent(activity, RelativeLayoutActivity.class);
+                break;
+            default:
+                screenIntent = new Intent(activity, LinearLayoutActivity.class);
+        }
+        return screenIntent;
+    }
+}
